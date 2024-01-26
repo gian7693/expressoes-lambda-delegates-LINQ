25777f39b05c79d3db7bcac12964690d53ed6dc1
Gian Makoto <gianmakoto@hotmail.com>
2024-01-26 14:21:00 -0300

    Uma experiencia com comparison

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Course.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Course // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            list.Sort(comp);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        //static int CompareProducts(Product p1, Product p2)
+        //{
+        //    return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        //}
+    }
+}
