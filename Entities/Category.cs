98fb1fa456aa20112f3569a1d2be6d81d4840169
Gian Makoto Fujimura Lopes <gianmakoto@hotmail.com>
2024-02-13 16:45:58 -0300

    Demo - LINQ com Lambda - PARTE 1

---
+++ Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Course.Entities
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+
+    }
+}
